666666b866688d7ce6768e180076a525ed446ae0
crimegod <dpk.exe@gmail.com>
2022-02-26 11:49:26 +0530

    changes 1

--- index.js
+++ index.js
@@ -1,6 +1,20 @@
 const express = require('express');
 const app = express();
 const port = 8000;
+const expressLayouts = require('express-ejs-layouts');
+const db = require('./config/mongoose');
+const cookieParser = require('cookie-parser');
+
+
+app.use(express.urlencoded());
+app.use(cookieParser());
+
+app.use(express.static('./assets'));
+app.use(expressLayouts);
+
+//extract style and script sub pages into layout
+app.set('layout extractStyles', true);
+app.set('layout extractScripts', true);
 
 //use express router
 app.use('/', require('./routes'));
@@ -8,6 +22,7 @@ app.use('/', require('./routes'));
 //set up view engine
 app.set('view engine', 'ejs');
 app.set('views', './views');
+
 
 
 app.listen(port, function(err){

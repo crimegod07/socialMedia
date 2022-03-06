55da5370e325482846e8c6b1ae455dd3a888d2af
crimegod <dpk.exe@gmail.com>
2022-03-07 02:40:21 +0530

    comments

--- controllers/users_controller.js
+++ controllers/users_controller.js
@@ -1,61 +1,67 @@
-const User =require('../models/user');
+const User = require('../models/user');
 
-module.exports.profile = function(req, res) {
-    if(req.cookies.user_id){
-      User.findById(req.cookies.user_id, function(err, user){
-          if(user){
-              return res.render('user_profile', {
-                  title: "User Profile",
-                  user: user
-              })
-          }
-          return res.redirect('/users/sign-up');
-      })
-    }else{
-        return res.redirect('/users/sign-in');
+
+module.exports.profile = function(req, res){
+    return res.render('user_profile', {
+        title: 'User Profile'
+    })
+}
+
+
+// render the sign up page
+module.exports.signUp = function(req, res){
+    if (req.isAuthenticated()){
+        return res.redirect('/users/profile');
     }
-}
 
-//render the sign up page
-module.exports.signUp = function(req, res) {
+
     return res.render('user_sign_up', {
-        title: "codeial | sign Up"
+        title: "Codeial | Sign Up"
     })
 }
 
-//render the sign in page
-module.exports.signIn = function(req, res) {
+
+// render the sign in page
+module.exports.signIn = function(req, res){
+
+    if (req.isAuthenticated()){
+        return res.redirect('/users/profile');
+    }
     return res.render('user_sign_in', {
-        title: "codeial | sign In"
+        title: "Codeial | Sign In"
     })
 }
 
-//get the sign up data
-module.exports.create = function(req, res) {
-    if(req.body.password != req.body.confirm_password){
+// get the sign up data
+module.exports.create = function(req, res){
+    if (req.body.password != req.body.confirm_password){
         return res.redirect('back');
     }
 
-    User.findOne( {email: req.body.email}, function(err, user){
-        if(err){
-            console.log('error in finding user in signing up'); return }
+    User.findOne({email: req.body.email}, function(err, user){
+        if(err){console.log('error in finding user in signing up'); return}
 
-            if(!user){
-                User.create(req.body, function(err, user){
-                    if(err){
-                        console.log('error in creating user while signing up'); return
-                    }
-                    return res.redirect('/user/sign-in');
-                })
-            }else{
-                return res.redirect('back');
-            }
+        if (!user){
+            User.create(req.body, function(err, user){
+                if(err){console.log('error in creating user while signing up'); return}
+
+                return res.redirect('/users/sign-in');
+            })
+        }else{
+            return res.redirect('back');
+        }
+
     });
 }
 
-//sign-in and create the session for the user
+
+// sign in and create a session for the user
 module.exports.createSession = function(req, res){
-   return res.redirect('/');
-   
-  
+    return res.redirect('/');
+}
+
+module.exports.destroySession = function(req, res){
+    req.logout();
+
+    return res.redirect('/');
 }
--- index.js
+++ index.js
@@ -1,54 +1,83 @@
 const express = require('express');
+const cookieParser = require('cookie-parser');
 const app = express();
 const port = 8000;
 const expressLayouts = require('express-ejs-layouts');
 const db = require('./config/mongoose');
-const cookieParser = require('cookie-parser');
 //used for session cookie
 const session = require('express-session');
-const passport = require('passport');
+var passport = require('passport');
 const passportLocal = require('./config/passport-local-strategy');
+const MongoStore = require('connect-mongo')(session);
+const sassMiddleware = require('node-sass-middleware');
 
 
-app.use(express.urlencoded());
-app.use(cookieParser());
 
-app.use(express.static('./assets'));
-app.use(expressLayouts);
+app.use(sassMiddleware({
+    src: './assets/scss',
+    dest: './assets/css',
+    debug: true,
+    outputStyle: 'expanded',
+    prefix:'/css'
+}));
 
-//extract style and script sub pages into layout
-app.set('layout extractStyles', true);
-app.set('layout extractScripts', true);
+ app.use(express.urlencoded());
+ app.use(cookieParser());
 
-//use express router
-app.use('/', require('./routes'));
+ app.use(express.static('./assets'));
+ app.use(expressLayouts);
 
-//set up view engine
-app.set('view engine', 'ejs');
-app.set('views', './views');
+// //extract style and script sub pages into layout
+ app.set('layout extractStyles', true);
+ app.set('layout extractScripts', true);
 
 
+
+ //set up view engine
+ app.set('view engine', 'ejs');
+ app.set('views', './views');
+
+
+ //mongo store is used to store the session cookie in the db
 app.use(session({
+    name:'codeial',
+    //TODO change the secret before deployment in production mode
+    secret:"blah",
+    saveUninitialized: false,
+    resave: false,
+    cookie: {
+        maxAge: (1000 * 60 * 100)
+    },
 
-   name: 'codial',
-   //To Do change the secret before deployment
-   secret: 'blah something',
-   saveUninitialized: false,
-   resave: false,
-   cookie: {
-       maxAge: (1000 * 60 * 100)
-   }
-}))
+    store: new MongoStore(
+        {
+            mongooseConnection: db,
+            autoRemove: 'disabled'
+
+        },
+
+        function(err){
+            console.log(err || 'connect-mongodb setup ok')
+        }
+    )
+
+}));
 
 app.use(passport.initialize());
 app.use(passport.session());
 
+app.use(passport.setAuthenticatedUser);
 
-app.listen(port, function(err){
+//use express router
+app.use('/', require('./routes'));
+
+
+
+ app.listen(port, function(err){
     if(err){
-        console.log('Error', err)
-        console.log(`Error in running the server: $(err)`);
-    }
+//         console.log('Error', err)
+       console.log(`Error in running the server: $(err)`);
+     }
 
     console.log(`server is running on port: ${port}` );
-})
+ });
